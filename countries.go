776239690272da03aeb418dc3587651c366c126d
Steve Kroll <steve.m.kroll@gmail.com>
2020-08-31 18:07:07 -0500

    generated complete list of supported countries

--- countries.go
+++ countries.go
@@ -1,123 +1,257 @@
 package countrycode
 
-// Countries defines the list of supported countries
-var Countries = []Country{US, CA, MX, GB, DK, FI, FR, DE, HU, IS, IT, NL, NO, PL, ES, SE, CH}
+// Country with ISO 3166 Alpha2, Alpha3 and Numeric Codes
+var (
+	AF = Country{Alpha2: "AF", Alpha3: "AFG", Numeric: "004"}
+	AX = Country{Alpha2: "AX", Alpha3: "ALA", Numeric: "248"}
+	AL = Country{Alpha2: "AL", Alpha3: "ALB", Numeric: "008"}
+	DZ = Country{Alpha2: "DZ", Alpha3: "DZA", Numeric: "012"}
+	AS = Country{Alpha2: "AS", Alpha3: "ASM", Numeric: "016"}
+	AD = Country{Alpha2: "AD", Alpha3: "AND", Numeric: "020"}
+	AO = Country{Alpha2: "AO", Alpha3: "AGO", Numeric: "024"}
+	AI = Country{Alpha2: "AI", Alpha3: "AIA", Numeric: "660"}
+	AQ = Country{Alpha2: "AQ", Alpha3: "ATA", Numeric: "010"}
+	AG = Country{Alpha2: "AG", Alpha3: "ATG", Numeric: "028"}
+	AR = Country{Alpha2: "AR", Alpha3: "ARG", Numeric: "032"}
+	AM = Country{Alpha2: "AM", Alpha3: "ARM", Numeric: "051"}
+	AW = Country{Alpha2: "AW", Alpha3: "ABW", Numeric: "533"}
+	AU = Country{Alpha2: "AU", Alpha3: "AUS", Numeric: "036"}
+	AT = Country{Alpha2: "AT", Alpha3: "AUT", Numeric: "040"}
+	AZ = Country{Alpha2: "AZ", Alpha3: "AZE", Numeric: "031"}
+	BS = Country{Alpha2: "BS", Alpha3: "BHS", Numeric: "044"}
+	BH = Country{Alpha2: "BH", Alpha3: "BHR", Numeric: "048"}
+	BD = Country{Alpha2: "BD", Alpha3: "BGD", Numeric: "050"}
+	BB = Country{Alpha2: "BB", Alpha3: "BRB", Numeric: "052"}
+	BY = Country{Alpha2: "BY", Alpha3: "BLR", Numeric: "112"}
+	BE = Country{Alpha2: "BE", Alpha3: "BEL", Numeric: "056"}
+	BZ = Country{Alpha2: "BZ", Alpha3: "BLZ", Numeric: "084"}
+	BJ = Country{Alpha2: "BJ", Alpha3: "BEN", Numeric: "204"}
+	BM = Country{Alpha2: "BM", Alpha3: "BMU", Numeric: "060"}
+	BT = Country{Alpha2: "BT", Alpha3: "BTN", Numeric: "064"}
+	BO = Country{Alpha2: "BO", Alpha3: "BOL", Numeric: "068"}
+	BQ = Country{Alpha2: "BQ", Alpha3: "BES", Numeric: "535"}
+	BA = Country{Alpha2: "BA", Alpha3: "BIH", Numeric: "070"}
+	BW = Country{Alpha2: "BW", Alpha3: "BWA", Numeric: "072"}
+	BV = Country{Alpha2: "BV", Alpha3: "BVT", Numeric: "074"}
+	BR = Country{Alpha2: "BR", Alpha3: "BRA", Numeric: "076"}
+	IO = Country{Alpha2: "IO", Alpha3: "IOT", Numeric: "086"}
+	BN = Country{Alpha2: "BN", Alpha3: "BRN", Numeric: "096"}
+	BG = Country{Alpha2: "BG", Alpha3: "BGR", Numeric: "100"}
+	BF = Country{Alpha2: "BF", Alpha3: "BFA", Numeric: "854"}
+	BI = Country{Alpha2: "BI", Alpha3: "BDI", Numeric: "108"}
+	CV = Country{Alpha2: "CV", Alpha3: "CPV", Numeric: "132"}
+	KH = Country{Alpha2: "KH", Alpha3: "KHM", Numeric: "116"}
+	CM = Country{Alpha2: "CM", Alpha3: "CMR", Numeric: "120"}
+	CA = Country{Alpha2: "CA", Alpha3: "CAN", Numeric: "124"}
+	KY = Country{Alpha2: "KY", Alpha3: "CYM", Numeric: "136"}
+	CF = Country{Alpha2: "CF", Alpha3: "CAF", Numeric: "140"}
+	TD = Country{Alpha2: "TD", Alpha3: "TCD", Numeric: "148"}
+	CL = Country{Alpha2: "CL", Alpha3: "CHL", Numeric: "152"}
+	CN = Country{Alpha2: "CN", Alpha3: "CHN", Numeric: "156"}
+	CX = Country{Alpha2: "CX", Alpha3: "CXR", Numeric: "162"}
+	CC = Country{Alpha2: "CC", Alpha3: "CCK", Numeric: "166"}
+	CO = Country{Alpha2: "CO", Alpha3: "COL", Numeric: "170"}
+	KM = Country{Alpha2: "KM", Alpha3: "COM", Numeric: "174"}
+	CD = Country{Alpha2: "CD", Alpha3: "COD", Numeric: "180"}
+	CG = Country{Alpha2: "CG", Alpha3: "COG", Numeric: "178"}
+	CK = Country{Alpha2: "CK", Alpha3: "COK", Numeric: "184"}
+	CR = Country{Alpha2: "CR", Alpha3: "CRI", Numeric: "188"}
+	CI = Country{Alpha2: "CI", Alpha3: "CIV", Numeric: "384"}
+	HR = Country{Alpha2: "HR", Alpha3: "HRV", Numeric: "191"}
+	CU = Country{Alpha2: "CU", Alpha3: "CUB", Numeric: "192"}
+	CW = Country{Alpha2: "CW", Alpha3: "CUW", Numeric: "531"}
+	CY = Country{Alpha2: "CY", Alpha3: "CYP", Numeric: "196"}
+	CZ = Country{Alpha2: "CZ", Alpha3: "CZE", Numeric: "203"}
+	DK = Country{Alpha2: "DK", Alpha3: "DNK", Numeric: "208"}
+	DJ = Country{Alpha2: "DJ", Alpha3: "DJI", Numeric: "262"}
+	DM = Country{Alpha2: "DM", Alpha3: "DMA", Numeric: "212"}
+	DO = Country{Alpha2: "DO", Alpha3: "DOM", Numeric: "214"}
+	EC = Country{Alpha2: "EC", Alpha3: "ECU", Numeric: "218"}
+	EG = Country{Alpha2: "EG", Alpha3: "EGY", Numeric: "818"}
+	SV = Country{Alpha2: "SV", Alpha3: "SLV", Numeric: "222"}
+	GQ = Country{Alpha2: "GQ", Alpha3: "GNQ", Numeric: "226"}
+	ER = Country{Alpha2: "ER", Alpha3: "ERI", Numeric: "232"}
+	EE = Country{Alpha2: "EE", Alpha3: "EST", Numeric: "233"}
+	SZ = Country{Alpha2: "SZ", Alpha3: "SWZ", Numeric: "748"}
+	ET = Country{Alpha2: "ET", Alpha3: "ETH", Numeric: "231"}
+	FK = Country{Alpha2: "FK", Alpha3: "FLK", Numeric: "238"}
+	FO = Country{Alpha2: "FO", Alpha3: "FRO", Numeric: "234"}
+	FJ = Country{Alpha2: "FJ", Alpha3: "FJI", Numeric: "242"}
+	FI = Country{Alpha2: "FI", Alpha3: "FIN", Numeric: "246"}
+	FR = Country{Alpha2: "FR", Alpha3: "FRA", Numeric: "250"}
+	GF = Country{Alpha2: "GF", Alpha3: "GUF", Numeric: "254"}
+	PF = Country{Alpha2: "PF", Alpha3: "PYF", Numeric: "258"}
+	TF = Country{Alpha2: "TF", Alpha3: "ATF", Numeric: "260"}
+	GA = Country{Alpha2: "GA", Alpha3: "GAB", Numeric: "266"}
+	GM = Country{Alpha2: "GM", Alpha3: "GMB", Numeric: "270"}
+	GE = Country{Alpha2: "GE", Alpha3: "GEO", Numeric: "268"}
+	DE = Country{Alpha2: "DE", Alpha3: "DEU", Numeric: "276"}
+	GH = Country{Alpha2: "GH", Alpha3: "GHA", Numeric: "288"}
+	GI = Country{Alpha2: "GI", Alpha3: "GIB", Numeric: "292"}
+	GR = Country{Alpha2: "GR", Alpha3: "GRC", Numeric: "300"}
+	GL = Country{Alpha2: "GL", Alpha3: "GRL", Numeric: "304"}
+	GD = Country{Alpha2: "GD", Alpha3: "GRD", Numeric: "308"}
+	GP = Country{Alpha2: "GP", Alpha3: "GLP", Numeric: "312"}
+	GU = Country{Alpha2: "GU", Alpha3: "GUM", Numeric: "316"}
+	GT = Country{Alpha2: "GT", Alpha3: "GTM", Numeric: "320"}
+	GG = Country{Alpha2: "GG", Alpha3: "GGY", Numeric: "831"}
+	GN = Country{Alpha2: "GN", Alpha3: "GIN", Numeric: "324"}
+	GW = Country{Alpha2: "GW", Alpha3: "GNB", Numeric: "624"}
+	GY = Country{Alpha2: "GY", Alpha3: "GUY", Numeric: "328"}
+	HT = Country{Alpha2: "HT", Alpha3: "HTI", Numeric: "332"}
+	HM = Country{Alpha2: "HM", Alpha3: "HMD", Numeric: "334"}
+	VA = Country{Alpha2: "VA", Alpha3: "VAT", Numeric: "336"}
+	HN = Country{Alpha2: "HN", Alpha3: "HND", Numeric: "340"}
+	HK = Country{Alpha2: "HK", Alpha3: "HKG", Numeric: "344"}
+	HU = Country{Alpha2: "HU", Alpha3: "HUN", Numeric: "348"}
+	IS = Country{Alpha2: "IS", Alpha3: "ISL", Numeric: "352"}
+	IN = Country{Alpha2: "IN", Alpha3: "IND", Numeric: "356"}
+	ID = Country{Alpha2: "ID", Alpha3: "IDN", Numeric: "360"}
+	IR = Country{Alpha2: "IR", Alpha3: "IRN", Numeric: "364"}
+	IQ = Country{Alpha2: "IQ", Alpha3: "IRQ", Numeric: "368"}
+	IE = Country{Alpha2: "IE", Alpha3: "IRL", Numeric: "372"}
+	IM = Country{Alpha2: "IM", Alpha3: "IMN", Numeric: "833"}
+	IL = Country{Alpha2: "IL", Alpha3: "ISR", Numeric: "376"}
+	IT = Country{Alpha2: "IT", Alpha3: "ITA", Numeric: "380"}
+	JM = Country{Alpha2: "JM", Alpha3: "JAM", Numeric: "388"}
+	JP = Country{Alpha2: "JP", Alpha3: "JPN", Numeric: "392"}
+	JE = Country{Alpha2: "JE", Alpha3: "JEY", Numeric: "832"}
+	JO = Country{Alpha2: "JO", Alpha3: "JOR", Numeric: "400"}
+	KZ = Country{Alpha2: "KZ", Alpha3: "KAZ", Numeric: "398"}
+	KE = Country{Alpha2: "KE", Alpha3: "KEN", Numeric: "404"}
+	KI = Country{Alpha2: "KI", Alpha3: "KIR", Numeric: "296"}
+	KP = Country{Alpha2: "KP", Alpha3: "PRK", Numeric: "408"}
+	KR = Country{Alpha2: "KR", Alpha3: "KOR", Numeric: "410"}
+	KW = Country{Alpha2: "KW", Alpha3: "KWT", Numeric: "414"}
+	KG = Country{Alpha2: "KG", Alpha3: "KGZ", Numeric: "417"}
+	LA = Country{Alpha2: "LA", Alpha3: "LAO", Numeric: "418"}
+	LV = Country{Alpha2: "LV", Alpha3: "LVA", Numeric: "428"}
+	LB = Country{Alpha2: "LB", Alpha3: "LBN", Numeric: "422"}
+	LS = Country{Alpha2: "LS", Alpha3: "LSO", Numeric: "426"}
+	LR = Country{Alpha2: "LR", Alpha3: "LBR", Numeric: "430"}
+	LY = Country{Alpha2: "LY", Alpha3: "LBY", Numeric: "434"}
+	LI = Country{Alpha2: "LI", Alpha3: "LIE", Numeric: "438"}
+	LT = Country{Alpha2: "LT", Alpha3: "LTU", Numeric: "440"}
+	LU = Country{Alpha2: "LU", Alpha3: "LUX", Numeric: "442"}
+	MO = Country{Alpha2: "MO", Alpha3: "MAC", Numeric: "446"}
+	MK = Country{Alpha2: "MK", Alpha3: "MKD", Numeric: "807"}
+	MG = Country{Alpha2: "MG", Alpha3: "MDG", Numeric: "450"}
+	MW = Country{Alpha2: "MW", Alpha3: "MWI", Numeric: "454"}
+	MY = Country{Alpha2: "MY", Alpha3: "MYS", Numeric: "458"}
+	MV = Country{Alpha2: "MV", Alpha3: "MDV", Numeric: "462"}
+	ML = Country{Alpha2: "ML", Alpha3: "MLI", Numeric: "466"}
+	MT = Country{Alpha2: "MT", Alpha3: "MLT", Numeric: "470"}
+	MH = Country{Alpha2: "MH", Alpha3: "MHL", Numeric: "584"}
+	MQ = Country{Alpha2: "MQ", Alpha3: "MTQ", Numeric: "474"}
+	MR = Country{Alpha2: "MR", Alpha3: "MRT", Numeric: "478"}
+	MU = Country{Alpha2: "MU", Alpha3: "MUS", Numeric: "480"}
+	YT = Country{Alpha2: "YT", Alpha3: "MYT", Numeric: "175"}
+	MX = Country{Alpha2: "MX", Alpha3: "MEX", Numeric: "484"}
+	FM = Country{Alpha2: "FM", Alpha3: "FSM", Numeric: "583"}
+	MD = Country{Alpha2: "MD", Alpha3: "MDA", Numeric: "498"}
+	MC = Country{Alpha2: "MC", Alpha3: "MCO", Numeric: "492"}
+	MN = Country{Alpha2: "MN", Alpha3: "MNG", Numeric: "496"}
+	ME = Country{Alpha2: "ME", Alpha3: "MNE", Numeric: "499"}
+	MS = Country{Alpha2: "MS", Alpha3: "MSR", Numeric: "500"}
+	MA = Country{Alpha2: "MA", Alpha3: "MAR", Numeric: "504"}
+	MZ = Country{Alpha2: "MZ", Alpha3: "MOZ", Numeric: "508"}
+	MM = Country{Alpha2: "MM", Alpha3: "MMR", Numeric: "104"}
+	NA = Country{Alpha2: "NA", Alpha3: "NAM", Numeric: "516"}
+	NR = Country{Alpha2: "NR", Alpha3: "NRU", Numeric: "520"}
+	NP = Country{Alpha2: "NP", Alpha3: "NPL", Numeric: "524"}
+	NL = Country{Alpha2: "NL", Alpha3: "NLD", Numeric: "528"}
+	NC = Country{Alpha2: "NC", Alpha3: "NCL", Numeric: "540"}
+	NZ = Country{Alpha2: "NZ", Alpha3: "NZL", Numeric: "554"}
+	NI = Country{Alpha2: "NI", Alpha3: "NIC", Numeric: "558"}
+	NE = Country{Alpha2: "NE", Alpha3: "NER", Numeric: "562"}
+	NG = Country{Alpha2: "NG", Alpha3: "NGA", Numeric: "566"}
+	NU = Country{Alpha2: "NU", Alpha3: "NIU", Numeric: "570"}
+	NF = Country{Alpha2: "NF", Alpha3: "NFK", Numeric: "574"}
+	MP = Country{Alpha2: "MP", Alpha3: "MNP", Numeric: "580"}
+	NO = Country{Alpha2: "NO", Alpha3: "NOR", Numeric: "578"}
+	OM = Country{Alpha2: "OM", Alpha3: "OMN", Numeric: "512"}
+	PK = Country{Alpha2: "PK", Alpha3: "PAK", Numeric: "586"}
+	PW = Country{Alpha2: "PW", Alpha3: "PLW", Numeric: "585"}
+	PS = Country{Alpha2: "PS", Alpha3: "PSE", Numeric: "275"}
+	PA = Country{Alpha2: "PA", Alpha3: "PAN", Numeric: "591"}
+	PG = Country{Alpha2: "PG", Alpha3: "PNG", Numeric: "598"}
+	PY = Country{Alpha2: "PY", Alpha3: "PRY", Numeric: "600"}
+	PE = Country{Alpha2: "PE", Alpha3: "PER", Numeric: "604"}
+	PH = Country{Alpha2: "PH", Alpha3: "PHL", Numeric: "608"}
+	PN = Country{Alpha2: "PN", Alpha3: "PCN", Numeric: "612"}
+	PL = Country{Alpha2: "PL", Alpha3: "POL", Numeric: "616"}
+	PT = Country{Alpha2: "PT", Alpha3: "PRT", Numeric: "620"}
+	PR = Country{Alpha2: "PR", Alpha3: "PRI", Numeric: "630"}
+	QA = Country{Alpha2: "QA", Alpha3: "QAT", Numeric: "634"}
+	RE = Country{Alpha2: "RE", Alpha3: "REU", Numeric: "638"}
+	RO = Country{Alpha2: "RO", Alpha3: "ROU", Numeric: "642"}
+	RU = Country{Alpha2: "RU", Alpha3: "RUS", Numeric: "643"}
+	RW = Country{Alpha2: "RW", Alpha3: "RWA", Numeric: "646"}
+	BL = Country{Alpha2: "BL", Alpha3: "BLM", Numeric: "652"}
+	SH = Country{Alpha2: "SH", Alpha3: "SHN", Numeric: "654"}
+	KN = Country{Alpha2: "KN", Alpha3: "KNA", Numeric: "659"}
+	LC = Country{Alpha2: "LC", Alpha3: "LCA", Numeric: "662"}
+	MF = Country{Alpha2: "MF", Alpha3: "MAF", Numeric: "663"}
+	PM = Country{Alpha2: "PM", Alpha3: "SPM", Numeric: "666"}
+	VC = Country{Alpha2: "VC", Alpha3: "VCT", Numeric: "670"}
+	WS = Country{Alpha2: "WS", Alpha3: "WSM", Numeric: "882"}
+	SM = Country{Alpha2: "SM", Alpha3: "SMR", Numeric: "674"}
+	ST = Country{Alpha2: "ST", Alpha3: "STP", Numeric: "678"}
+	SA = Country{Alpha2: "SA", Alpha3: "SAU", Numeric: "682"}
+	SN = Country{Alpha2: "SN", Alpha3: "SEN", Numeric: "686"}
+	RS = Country{Alpha2: "RS", Alpha3: "SRB", Numeric: "688"}
+	SC = Country{Alpha2: "SC", Alpha3: "SYC", Numeric: "690"}
+	SL = Country{Alpha2: "SL", Alpha3: "SLE", Numeric: "694"}
+	SG = Country{Alpha2: "SG", Alpha3: "SGP", Numeric: "702"}
+	SX = Country{Alpha2: "SX", Alpha3: "SXM", Numeric: "534"}
+	SK = Country{Alpha2: "SK", Alpha3: "SVK", Numeric: "703"}
+	SI = Country{Alpha2: "SI", Alpha3: "SVN", Numeric: "705"}
+	SB = Country{Alpha2: "SB", Alpha3: "SLB", Numeric: "090"}
+	SO = Country{Alpha2: "SO", Alpha3: "SOM", Numeric: "706"}
+	ZA = Country{Alpha2: "ZA", Alpha3: "ZAF", Numeric: "710"}
+	GS = Country{Alpha2: "GS", Alpha3: "SGS", Numeric: "239"}
+	SS = Country{Alpha2: "SS", Alpha3: "SSD", Numeric: "728"}
+	ES = Country{Alpha2: "ES", Alpha3: "ESP", Numeric: "724"}
+	LK = Country{Alpha2: "LK", Alpha3: "LKA", Numeric: "144"}
+	SD = Country{Alpha2: "SD", Alpha3: "SDN", Numeric: "729"}
+	SR = Country{Alpha2: "SR", Alpha3: "SUR", Numeric: "740"}
+	SJ = Country{Alpha2: "SJ", Alpha3: "SJM", Numeric: "744"}
+	SE = Country{Alpha2: "SE", Alpha3: "SWE", Numeric: "752"}
+	CH = Country{Alpha2: "CH", Alpha3: "CHE", Numeric: "756"}
+	SY = Country{Alpha2: "SY", Alpha3: "SYR", Numeric: "760"}
+	TW = Country{Alpha2: "TW", Alpha3: "TWN", Numeric: "158"}
+	TJ = Country{Alpha2: "TJ", Alpha3: "TJK", Numeric: "762"}
+	TZ = Country{Alpha2: "TZ", Alpha3: "TZA", Numeric: "834"}
+	TH = Country{Alpha2: "TH", Alpha3: "THA", Numeric: "764"}
+	TL = Country{Alpha2: "TL", Alpha3: "TLS", Numeric: "626"}
+	TG = Country{Alpha2: "TG", Alpha3: "TGO", Numeric: "768"}
+	TK = Country{Alpha2: "TK", Alpha3: "TKL", Numeric: "772"}
+	TO = Country{Alpha2: "TO", Alpha3: "TON", Numeric: "776"}
+	TT = Country{Alpha2: "TT", Alpha3: "TTO", Numeric: "780"}
+	TN = Country{Alpha2: "TN", Alpha3: "TUN", Numeric: "788"}
+	TR = Country{Alpha2: "TR", Alpha3: "TUR", Numeric: "792"}
+	TM = Country{Alpha2: "TM", Alpha3: "TKM", Numeric: "795"}
+	TC = Country{Alpha2: "TC", Alpha3: "TCA", Numeric: "796"}
+	TV = Country{Alpha2: "TV", Alpha3: "TUV", Numeric: "798"}
+	UG = Country{Alpha2: "UG", Alpha3: "UGA", Numeric: "800"}
+	UA = Country{Alpha2: "UA", Alpha3: "UKR", Numeric: "804"}
+	AE = Country{Alpha2: "AE", Alpha3: "ARE", Numeric: "784"}
+	GB = Country{Alpha2: "GB", Alpha3: "GBR", Numeric: "826"}
+	UM = Country{Alpha2: "UM", Alpha3: "UMI", Numeric: "581"}
+	US = Country{Alpha2: "US", Alpha3: "USA", Numeric: "840"}
+	UY = Country{Alpha2: "UY", Alpha3: "URY", Numeric: "858"}
+	UZ = Country{Alpha2: "UZ", Alpha3: "UZB", Numeric: "860"}
+	VU = Country{Alpha2: "VU", Alpha3: "VUT", Numeric: "548"}
+	VE = Country{Alpha2: "VE", Alpha3: "VEN", Numeric: "862"}
+	VN = Country{Alpha2: "VN", Alpha3: "VNM", Numeric: "704"}
+	VG = Country{Alpha2: "VG", Alpha3: "VGB", Numeric: "092"}
+	VI = Country{Alpha2: "VI", Alpha3: "VIR", Numeric: "850"}
+	WF = Country{Alpha2: "WF", Alpha3: "WLF", Numeric: "876"}
+	EH = Country{Alpha2: "EH", Alpha3: "ESH", Numeric: "732"}
+	YE = Country{Alpha2: "YE", Alpha3: "YEM", Numeric: "887"}
+	ZM = Country{Alpha2: "ZM", Alpha3: "ZMB", Numeric: "894"}
+	ZW = Country{Alpha2: "ZW", Alpha3: "ZWE", Numeric: "716"}
+)
 
-// US United States
-var US = Country{
-	Alpha2:  "US",
-	Alpha3:  "USA",
-	Numeric: "840",
-}
-
-// CA Canada
-var CA = Country{
-	Alpha2:  "CA",
-	Alpha3:  "CAN",
-	Numeric: "124",
-}
-
-// MX Mexico
-var MX = Country{
-	Alpha2:  "MX",
-	Alpha3:  "MEX",
-	Numeric: "484",
-}
-
-// GB Great Britain
-var GB = Country{
-	Alpha2:  "GB",
-	Alpha3:  "GBR",
-	Numeric: "826",
-}
-
-// DK Denmark
-var DK = Country{
-	Alpha2:  "DK",
-	Alpha3:  "DNK",
-	Numeric: "208",
-}
-
-// FI Finland
-var FI = Country{
-	Alpha2:  "FI",
-	Alpha3:  "FIN",
-	Numeric: "246",
-}
-
-// FR France
-var FR = Country{
-	Alpha2:  "FR",
-	Alpha3:  "FRA",
-	Numeric: "250",
-}
-
-// DE Germany
-var DE = Country{
-	Alpha2:  "DE",
-	Alpha3:  "DEU",
-	Numeric: "276",
-}
-
-// HU Hungary
-var HU = Country{
-	Alpha2:  "HU",
-	Alpha3:  "HUN",
-	Numeric: "348",
-}
-
-// IS Iceland
-var IS = Country{
-	Alpha2:  "IS",
-	Alpha3:  "ISL",
-	Numeric: "352",
-}
-
-// IT Italy
-var IT = Country{
-	Alpha2:  "IT",
-	Alpha3:  "ITA",
-	Numeric: "380",
-}
-
-// NL Netherlands
-var NL = Country{
-	Alpha2:  "NL",
-	Alpha3:  "NLD",
-	Numeric: "528",
-}
-
-// NO Norway
-var NO = Country{
-	Alpha2:  "NO",
-	Alpha3:  "NOR",
-	Numeric: "578",
-}
-
-// PL Poland
-var PL = Country{
-	Alpha2:  "PL",
-	Alpha3:  "POL",
-	Numeric: "616",
-}
-
-// ES Spain
-var ES = Country{
-	Alpha2:  "ES",
-	Alpha3:  "ESP",
-	Numeric: "724",
-}
-
-// SE Sweden
-var SE = Country{
-	Alpha2:  "SE",
-	Alpha3:  "SWE",
-	Numeric: "752",
-}
-
-// CH Switzerland
-var CH = Country{
-	Alpha2:  "CH",
-	Alpha3:  "CHE",
-	Numeric: "756",
-}
+// Countries is the complete list of ISO 3166 Countries
+var Countries = []Country{AF, AX, AL, DZ, AS, AD, AO, AI, AQ, AG, AR, AM, AW, AU, AT, AZ, BS, BH, BD, BB, BY, BE, BZ, BJ, BM, BT, BO, BQ, BA, BW, BV, BR, IO, BN, BG, BF, BI, CV, KH, CM, CA, KY, CF, TD, CL, CN, CX, CC, CO, KM, CD, CG, CK, CR, CI, HR, CU, CW, CY, CZ, DK, DJ, DM, DO, EC, EG, SV, GQ, ER, EE, SZ, ET, FK, FO, FJ, FI, FR, GF, PF, TF, GA, GM, GE, DE, GH, GI, GR, GL, GD, GP, GU, GT, GG, GN, GW, GY, HT, HM, VA, HN, HK, HU, IS, IN, ID, IR, IQ, IE, IM, IL, IT, JM, JP, JE, JO, KZ, KE, KI, KP, KR, KW, KG, LA, LV, LB, LS, LR, LY, LI, LT, LU, MO, MK, MG, MW, MY, MV, ML, MT, MH, MQ, MR, MU, YT, MX, FM, MD, MC, MN, ME, MS, MA, MZ, MM, NA, NR, NP, NL, NC, NZ, NI, NE, NG, NU, NF, MP, NO, OM, PK, PW, PS, PA, PG, PY, PE, PH, PN, PL, PT, PR, QA, RE, RO, RU, RW, BL, SH, KN, LC, MF, PM, VC, WS, SM, ST, SA, SN, RS, SC, SL, SG, SX, SK, SI, SB, SO, ZA, GS, SS, ES, LK, SD, SR, SJ, SE, CH, SY, TW, TJ, TZ, TH, TL, TG, TK, TO, TT, TN, TR, TM, TC, TV, UG, UA, AE, GB, UM, US, UY, UZ, VU, VE, VN, VG, VI, WF, EH, YE, ZM, ZW}
